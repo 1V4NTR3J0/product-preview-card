a5e622ec3cebe3eaab4f15192e2f755bbc3773a8
Ivan Trejo <trejo.ivan.c@gmail.com>
2022-10-07 21:27:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Product preview card component solution
+
+This is a solution to the [Product preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover and focus states for interactive elements
+
+### Screenshot
+
+![](./product-preview-screenshot.jpg)
+
+### Links
+
+- Live Site URL: [Live site](https://1v4ntr3j0.github.io/product-preview-card)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Scripting JS
+
+## Author
+
+- Frontend Mentor - [@1V4NTR3J0](https://www.frontendmentor.io/profile/1V4NTR3J0)
---
+++ src/js/app.js
@@ -0,0 +1,47 @@
+(() => {
+  const details = document.querySelector("#details");
+
+  window.addEventListener("DOMContentLoaded", () => {
+    buildCard();
+  });
+
+  function buildCard() {
+    const label = document.createElement("h5");
+    label.textContent = "Perfume";
+
+    const title = document.createElement("h2");
+    title.textContent = "Gabrielle Essence Eau De Parfum";
+
+    const description = document.createElement("p");
+    description.classList.add("description");
+    description.textContent =
+      "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.";
+
+    const divPrice = document.createElement("div");
+
+    const price = document.createElement("p");
+    price.textContent = "$149.99";
+    price.classList.add("price");
+
+    const exPrice = document.createElement("span");
+    exPrice.textContent = "$169.99";
+    exPrice.classList.add("exPrice");
+
+    price.appendChild(exPrice);
+    divPrice.appendChild(price);
+
+    const button = document.createElement("button");
+    button.textContent = "Add to Cart";
+
+    const icon = document.createElement("img");
+    icon.src = "/images/icon-cart.svg";
+    icon.alt = "Add to Cart";
+    button.append(icon);
+
+    details.appendChild(label);
+    details.appendChild(title);
+    details.appendChild(description);
+    details.appendChild(divPrice);
+    details.appendChild(button);
+  }
+})();
